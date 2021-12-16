170de68044b41375847707c945a9d87849b01d8c
shaongitt <83664154+shaongitt@users.noreply.github.com>
2021-12-16 11:00:53 -0500

    Create FLOW008.cpp

---
+++ codechef/FLOW008.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n,num;
+cin >> n;
+
+while(n--){
+
+  num = 0;
+  cin >> num;
+  if (num<10){
+
+    cout << "Thanks for helping Chef!" <<endl;
+
+  }
+  else{
+
+    cout <<"-1"<<endl;
+  }
+
+
+}
+
+
+}
