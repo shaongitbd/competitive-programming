94d59030561b8da15d6c5de7c8853becc036ae9f
shaongitt <83664154+shaongitt@users.noreply.github.com>
2021-12-16 11:02:01 -0500

    Create CHOPRT.cpp

---
+++ codechef/CHOPRT.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n,num1, num2;
+cin >>n;
+
+while(n--){
+
+ cin >> num1;
+ cin >> num2;
+
+ if (num1<num2){
+
+    cout << "<"<<endl;
+ }
+ else if (num1>num2){
+
+    cout << ">" <<endl;
+ }
+ else{
+
+    cout<< "="<<endl;
+
+ }
+
+
+}
+
+
+}
