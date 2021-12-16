937db6bd24ee85185eecc16065ee0345fa089b03
shaongitt <83664154+shaongitt@users.noreply.github.com>
2021-12-16 11:00:09 -0500

    Create FLOW002.cpp

---
+++ codechef/FLOW002.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int n, num1,num2;
+cin >> n;
+
+ while(n--){
+
+   cin >> num1;
+   cin >> num2;
+   cout << num1%num2 <<endl;
+
+
+
+ }
+
+}
