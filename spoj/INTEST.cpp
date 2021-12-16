f156a58119f0f1f77ee4bb875f6411ed68207420
shaongitt <83664154+shaongitt@users.noreply.github.com>
2021-12-16 11:04:28 -0500

    Create INTEST.cpp

---
+++ spoj/INTEST.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n, k, i, counter;
+cin >>n;
+cin >> k;
+counter = 0;
+
+while(n--){
+
+
+  cin >> i;
+
+  if(i%k==0){
+    counter +=1;
+  }
+
+
+
+}
+cout << counter << endl;
+
+}
