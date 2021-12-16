4e557ac1c46122b7b63cf811661dec2a09bede09
shaongitt <83664154+shaongitt@users.noreply.github.com>
2021-12-16 10:58:08 -0500

    Create FLOW001.cpp

---
+++ codechef/FLOW001.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+int n, num1, num2, sum;
+using namespace std;
+
+int main(){
+  cin >> n;
+  while(n--){
+    sum=0;
+    cin >> num1;
+    cin >> num2;
+    sum = num1+num2;
+    cout << sum <<endl;
+}
+}
